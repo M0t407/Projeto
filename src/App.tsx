b375ce85ede99539db9b3a0898ac55a731c82241
Moacyr <netomoacyrmn@gmail.com>
2022-06-21 19:27:28 -0300

    Atualização pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
-import Principal from './components/Principal';
+import Principal from './components/Moldura';
 import Contacts from './pages/Contacts';
+import Cadastro from './pages/Cadastro';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login />} />
+          <Route path='/login' element={<Login></Login>} />
           <Route path='/' element={<Principal><Dashboard /></Principal>} />
           <Route path='/contacts' element={<Principal><Contacts /></Principal>} />
+          <Route path='/cadastro' element={<Principal><Cadastro /></Principal>} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Cadastro.tsx
@@ -0,0 +1,9 @@
+const Cadastro = function () {
+    return (
+        <>
+        Cadastro
+        </>
+    );
+};
+
+export default Cadastro;
